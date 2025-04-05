1c76876977713038c36603efb86451cd7773313a
David Meunier <david_meunier_79@hotmail.fr>
2025-04-05 18:09:21 +0200

    testing

--- Fenetre/Fenetre.h
+++ Fenetre/Fenetre.h
@@ -12,7 +12,17 @@
 //#include "cmask"
 
 // NEW
+#ifdef __STDC_ALLOC_LIB__
+#define __STDC_WANT_LIB_EXT2__ 1
+#else
 #define _POSIX_C_SOURCE 200809L
+#endif
+#include <string.h>
+#include <assert.h>
+#include <stdlib.h>
+
+
+
 
 #include <iostream>
 #include <string>
