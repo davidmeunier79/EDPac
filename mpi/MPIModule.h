b275fcc2dd6ceb465a5039b2249f1b9d6704cfd5
David Meunier <david_meunier_79@hotmail.fr>
2025-05-01 11:18:26 +0200

    Update MPIModule.h
    modif with copilot

--- mpi/MPIModule.h
+++ mpi/MPIModule.h
@@ -4,16 +4,10 @@
 #define _MPI_MODULE_H
 
 // Local Includes
-
-#include "MpiGlobals.h"
-
 #include "../Define.h"
-#include "../DefineMPI.h"
-
 #include "InputBuffer.h"
 #include "OutputBuffer.h"
 
-//#include "SynchroBuffer.h"
 /******************************************************************************************************************************************/
 /***************************************************** Classe MPIModule *****************************************************************/
 /******************************************************************************************************************************************/
@@ -21,49 +15,48 @@
 class MPIModule
 {
 protected:
-	// Nombre de buffers dans le module
-	int nbInputBuffers;
-	int nbOutputBuffers;
+    // Nombre de buffers dans le module
+    int nbInputBuffers;
+    int nbOutputBuffers;
 
-	// Buffers du module
-	InputBuffer** inputBuffers;
-	OutputBuffer** outputBuffers;
+    // Buffers du module
+    InputBuffer** inputBuffers;
+    OutputBuffer** outputBuffers;
 
 public:
-	MPIModule();
-	MPIModule(MPIModule& MPIm);
-	MPIModule(int newNbInputBuffers, int newNbOutputBuffers);
-	
-	~MPIModule();
+    MPIModule();
+    MPIModule(MPIModule& MPIm);
+    MPIModule(int newNbInputBuffers, int newNbOutputBuffers);
 
-	void initMPIModule();
-	void freeMPIModule();
+    ~MPIModule();
 
-	// Reception des inputs
-	bool testAnyInputs();
-	int testSomeInputs(int* recus);
-	int waitSomeInputs(int* recus);
-	bool waitAllInputs();
-	//bool testAllInputs();
+    void initModule();
+    void freeModule();
 
-	// Envoi des outputs
-	void testAllOutputs();
-	void testAllInputs();
+    // Reception des inputs
+    bool testAnyInputs();
+    int testSomeInputs(int* recus);
+    int waitSomeInputs(int* recus);
+    bool waitAllInputs();
 
-	int testSomeOutputs(int* sended);
-	int waitSomeOutputs(int* sended);
-	bool waitAllOutputs();
+    // Envoi des outputs
+    void testAllOutputs();
+    void testAllInputs();
 
-	// Utilisation d'active
-	virtual bool isReady();
-	
-	// Initilisation des buffers
-	virtual void initInputBuffers();
-	virtual void initOutputBuffers();
+    int testSomeOutputs(int* sended);
+    int waitSomeOutputs(int* sended);
+    bool waitAllOutputs();
 
-	// Envoi et reception
-	virtual void receiveInputs();
-	virtual void sendOutputs();
+    // Utilisation d'active
+    virtual bool isReady();
+
+    // Initilisation des buffers
+    virtual void initInputBuffers();
+    virtual void initOutputBuffers();
+
+    // Envoi et reception
+    virtual void receiveInputs();
+    virtual void sendOutputs();
 };
 
 #endif
